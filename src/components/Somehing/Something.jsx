91083b42a33bad1653bab69f5e9f100cba9b9ec5
Annie Eliasson <annie.eliasson@gmail.com>
2024-02-25 17:42:26 +0100

    banderoll?

--- src/components/Somehing/Something.jsx
+++ src/components/Somehing/Something.jsx
@@ -4,6 +4,10 @@ const Something = () => {
     return(
         <div className="Something">
             <h2>Something</h2>
+
+            <div className="banderoll">
+                <p className="banderoll-text">"Det viktiga är inte vad man gör, utan att man gör det med stil." - Lars Demian</p>
+            </div>
         </div>
     )
 }
